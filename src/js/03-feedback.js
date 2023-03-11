e655c8bdbae18f1ce5514f46bb0234e1f2464ce6
VladyslavDmytrovychPavlenko <onlyforonlineworkvlad@gmail.com>
2023-03-11 13:07:14 +0200

    fix mentor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,36 +7,39 @@ const form = document.querySelector('.feedback-form');
 // знаходимо поле введення тексту повідомлення
 const textarea = document.querySelector('.feedback-form textarea');
 // знаходимо поле введення електронної пошти
-const input = document.querySelector('input');
-const formData = {}; // об'єкт, в якому будуть зберігатися дані форми
-populateTextarea(); // заповнюємо поля форми даними з локального сховища (якщо такі є)
-// Додаємо обробник події на відправку форми
+const input = document.querySelector('.feedback-form input[type="email"]');
+// Заповнення форми збереженими даними, якщо вони є
+populateFormFields();
+// Додавання обробників подій на форму
 form.addEventListener('submit', onFormSubmit);
-// Додаємо обмежувач частоти виконання функції на введення тексту повідомлення
-form.addEventListener('input', throttle(onTextareaInput, 500));
-// Додаємо обробник події на введення даних у поля форми
-form.addEventListener('input', evt => {
-  formData[evt.target.name] = evt.target.value; // зберігаємо дані у формі в об'єкт formData
-});
-// Обробник події на введення тексту повідомлення
-function onTextareaInput(evt) {
+form.addEventListener('input', throttle(onFormInput, 500));
+// Обробник вводу даних у форму
+function onFormInput(evt) {
+  // Отримання збережених даних або створення порожнього об'єкту, якщо збережених даних немає
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  // Додавання/оновлення даних у формі
+  formData[evt.target.name] = evt.target.value;
+  // Збереження даних у localStorage
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
+// Обробник відправки форми
 function onFormSubmit(evt) {
-  evt.preventDefault(); // зупиняємо дійсну відправку форми на сервер
-  evt.target.reset(); // очищаємо поля форми
-  const objData = JSON.parse(localStorage.getItem(STORAGE_KEY)); // отримуємо дані форми з локального сховища
-  localStorage.removeItem(STORAGE_KEY); // видаляємо дані з локального сховища
-  console.log(formData); // виводимо дані з форми у консоль
+  // Зупинка стандартної відправки форми
+  evt.preventDefault();
+  // Очистка форми
+  evt.target.reset();
+  // Отримання збережених даних або створення порожнього об'єкту, якщо збережених даних немає
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  // Видалення збережених даних з localStorage
+  localStorage.removeItem(STORAGE_KEY);
+  // Виведення даних у консоль (або відправка даних на сервер)
+  console.log(formData);
 }
-// Функція для заповнення полів форми даними з локального сховища
-function populateTextarea() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)); // отримуємо дані форми
-
-  if (savedMessage === null) {
-    return; // якщо немає даних у локальному сховищі, виходимо з функції
+// Заповнення полів форми збереженими даними
+function populateFormFields() {
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (formData) {
+    textarea.value = formData.message || '';
+    input.value = formData.email || '';
   }
-  textarea.value = savedMessage['message'] || ''; // заповнюємо поле введення тексту повідомлення
-  input.value = savedMessage['email'] || ''; // заповнюємо поле введення електронної пошти
 }
